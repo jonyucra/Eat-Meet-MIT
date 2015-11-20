bba61514ba0f01584a70e29b785489c81901d15a
Carlos Caldera <ccaldera@mit.edu>
2015-11-20 17:54:50 -0500

    added routing for users and requests; modified templates

--- Eat_Meet_MIT/public/javascripts/users.js
+++ Eat_Meet_MIT/public/javascripts/users.js
@@ -4,28 +4,46 @@
   // Event handler for whenever a new user logs in
   $(document).on('submit', '#loginForm', function(evt) {
       evt.preventDefault();
-      //$('.error').text(response.err);
-      loadPage('homepage');
-      // TODO add more functionality
+      $.post(
+          '/users/login',
+          helpers.getFormData(this)
+      ).done(function(response) {
+          currentUser = response.content.user;
+          loadHomePage();
+      }).fail(function(responseObject) {
+          var response = $.parseJSON(responseObject.responseText);
+          $('.error').text(response.err);
+      });
   });
 
   // Event handler for whenever a new user registers 
   $(document).on('submit', '#registerForm', function(evt) {
       evt.preventDefault();
-      // TODO add more functionality
-      //var formData = helpers.getFormData(this);
-      //if (formData.password !== formData.confirm) {
-      //    $('.error').text('Password and confirmation do not match!');
-      //    return;
-      //}
-      //$('.error').text(response.err);
-      loadPage('homepage');
+      var formData = helpers.getFormData(this);
+      console.log(formData);
+      if (formData.password !== formData.confirm) {
+          $('.error').text('Password and confirmation do not match!');
+          return;
+      }
+      if (formData.email.indexOf('@mit.edu') == -1) {
+          $('.error').text('Email is not an MIT email!');
+          return;
+      }
+      delete formData['confirm'];
+      $.post(
+          '/users',
+          formData
+      ).done(function(response) {
+          loadHomePage();
+      }).fail(function(responseObject) {
+          var response = $.parseJSON(responseObject.responseText);
+          $('.error').text(response.err);
+      });
   });
 
   // Event handler for whenever a user asks for a request
   $(document).on('click', '#makeRequest', function(evt) {
       loadPage('request');
-      // TODO add more functionality
   }); 
 
   // Event handler for whenever a user asks to see network 
@@ -37,8 +55,14 @@
   // Event handler for whevner a user logs out
   $(document).on('click', '#logoutLink', function(evt) {
       evt.preventDefault();
-      //$('.error').text(response.err);
-      loadPage('index');
-      // TODO make sure functionality met 
+      $.post(
+          '/users/logout'
+      ).done(function(response) {
+          currentUser = undefined;
+          loadHomePage();
+      }).fail(function(responseObject) {
+          var response = $.parseJSON(responseObject.responseText);
+          $('.error').text(response.err);
+      });
   });
 })();
