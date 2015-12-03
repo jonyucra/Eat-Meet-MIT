a7a81ed49c25cc51e1d7f9676ae21de92a39c179
John <jdonavon@mit.edu>
2015-12-03 03:12:31 -0500

    Worked on network and messages UI.

--- public/javascripts/templates.js
+++ public/javascripts/templates.js
@@ -1,69 +1,48 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-<<<<<<< HEAD
-templates['conversation'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
-
-  return "    <div class=\"message\">\r\n    <p>"
-    + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data}) : helper)))
-    + ": "
-    + alias4(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"content","hash":{},"data":data}) : helper)))
-    + "</p>\r\n";
-},"3":function(container,depth0,helpers,partials,data) {
-    return "    <p>No message yet. Start Chatting!</p>\r\n";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-=======
 templates['conversation'] = template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing;
+    var stack1;
 
-  return "    <p>sent at "
-    + container.escapeExpression(((helper = (helper = helpers.create_time || (depth0 != null ? depth0.create_time : depth0)) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"create_time","hash":{},"data":data}) : helper)))
-    + "</p>\n"
-    + ((stack1 = (helpers.ifEq || (depth0 && depth0.ifEq) || alias2).call(alias1,(depth0 != null ? depth0.author : depth0),"===",(depths[1] != null ? depths[1].currentUser : depths[1]),{"name":"ifEq","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.program(7, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "");
+  return "          \r\n"
+    + ((stack1 = (helpers.ifEq || (depth0 && depth0.ifEq) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.author : depth0),"===",(depths[1] != null ? depths[1].currentUser : depths[1]),{"name":"ifEq","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.program(7, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "");
 },"2":function(container,depth0,helpers,partials,data,blockParams,depths) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing;
+    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "      <div class=\"author_message\">\n      "
-    + container.escapeExpression(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"content","hash":{},"data":data}) : helper)))
-    + "\n      </div>\n"
+  return "            <div class=\"author_message\">\r\n              "
+    + alias4(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"content","hash":{},"data":data}) : helper)))
+    + "\r\n              <p class=\"messageDetail\">sent at "
+    + alias4(((helper = (helper = helpers.create_time || (depth0 != null ? depth0.create_time : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"create_time","hash":{},"data":data}) : helper)))
+    + "</p>\r\n            </div>\r\n\r\n"
     + ((stack1 = (helpers.ifEq || (depth0 && depth0.ifEq) || alias2).call(alias1,(depth0 != null ? depth0._id : depth0),"===",(depths[1] != null ? depths[1].last_message_id : depths[1]),{"name":"ifEq","hash":{},"fn":container.program(3, data, 0, blockParams, depths),"inverse":container.program(5, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "");
 },"3":function(container,depth0,helpers,partials,data,blockParams,depths) {
-    return "        <div class= \"message_status\">\n        <p>"
+    return "              <div class= \"message_status\">\r\n                <p class=\"messageDetail\">"
     + container.escapeExpression(container.lambda((depths[1] != null ? depths[1].last_read_status : depths[1]), depth0))
-    + "</p>\n        </div>\n";
+    + "</p>\r\n              </div>\r\n";
 },"5":function(container,depth0,helpers,partials,data) {
     return "";
 },"7":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "      <div class=\"receiver_message\">\n      "
+  return "            <div class=\"receiver_message\">\r\n            "
     + container.escapeExpression(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"content","hash":{},"data":data}) : helper)))
-    + "\n      </div>\n";
+    + "\r\n            </div>\r\n";
 },"9":function(container,depth0,helpers,partials,data) {
-    return "    <p>No message yet. Start Chatting!</p>\n";
+    return "          <p>No message yet. Start Chatting!</p>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
->>>>>>> 5822c8c2134934bb2949adbff95e14606301db0a
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<div id=\"conversation\" conversationId="
     + alias4(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"_id","hash":{},"data":data}) : helper)))
-    + ">\r\n  <h2>Eat, Meet, MIT</h2>\r\n  <p>Hi there "
+    + ">\r\n  <div class=\"jumbotron text-center\">\r\n    <h1>Eat, Meet, MIT</h1>\r\n    <p>Hi there "
     + alias4(((helper = (helper = helpers.currentUser || (depth0 != null ? depth0.currentUser : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"currentUser","hash":{},"data":data}) : helper)))
-    + ". (<a id=\"logoutLink\" href=\"#\">logout</a>)</p>\r\n  <p>Below are all conversations between you and "
+    + ". (<a id=\"logoutLink\" href=\"#\">logout</a>)</p>\r\n    <p>Below are all conversations between you and "
     + alias4(((helper = (helper = helpers.receiverUser || (depth0 != null ? depth0.receiverUser : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"receiverUser","hash":{},"data":data}) : helper)))
-<<<<<<< HEAD
-    + "</p>\r\n\r\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.messageArray : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
-    + "\r\n  <form id=\"new_message\" action=\"#\"> \r\n  <input type=\"text\" name=\"new_message_input\" id=\"new_message_input\">\r\n  <input type=\"submit\" id=\"submit_new_message\" value=\"Send\">\r\n  </form>\r\n  <br>\r\n\r\n  <input id=\"seeNetwork\" class=\"generalBtn\" type=\"button\" value=\"Back Network\">\r\n  <input id=\"homeBtn\" class=\"generalBtn\" type=\"button\" value=\"Back Home\">\r\n\r\n</div>\r\n";
-},"useData":true});
-=======
-    + "</p>\n\n  <div class = \"conversation_receriver_name\"> "
+    + "</p>\r\n  </div>\r\n\r\n  <div class=\"row text-center\">\r\n    \r\n    <div class=\"col-md-4 col-md-offset-4\">\r\n    \r\n      <div> <b class=\"messagePerson\">"
     + alias4(((helper = (helper = helpers.receiverUser || (depth0 != null ? depth0.receiverUser : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"receiverUser","hash":{},"data":data}) : helper)))
-    + " </div>\n  <div class = \"chat_box\">\n"
+    + "</b> </div>\r\n\r\n      <div class = \"chat_box\">\r\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.messageArray : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.program(9, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "")
-    + "  </div>\n  \n  <form id=\"new_message\" action=\"#\"> \n  <input type=\"text\" name=\"new_message_input\" id=\"new_message_input\">\n  <input type=\"submit\" id=\"submit_new_message\" value=\"Send\">\n  </form>\n  <br>\n\n  <input id=\"seeNetwork\" class=\"generalBtn\" type=\"button\" value=\"Back Network\">\n  <input id=\"homeBtn\" class=\"generalBtn\" type=\"button\" value=\"Back Home\">\n\n</div>\n";
+    + "      </div>\r\n        \r\n        <form id=\"new_message\" action=\"#\" class=\"form-inline\">\r\n          <div class=\"form-group\">\r\n            <input type=\"text\" name=\"new_message_input\" id=\"new_message_input\" class=\"form-control\">\r\n            <button class=\"btn btn-primary btn-sm\" id=\"submit_new_message\">Send</button>\r\n          </div>\r\n        </form>\r\n        <br>\r\n\r\n        \r\n        <button id=\"seeNetwork\" class=\"btn btn-primary btn-lg\">Back Network</button>\r\n        <button id=\"homeBtn\" class=\"btn btn-primary btn-lg\">Back Home</button>\r\n\r\n    </div>\r\n  </div>\r\n</div>\r\n";
 },"useData":true,"useDepths":true});
->>>>>>> 5822c8c2134934bb2949adbff95e14606301db0a
 templates['homepage'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing;
 
@@ -108,64 +87,47 @@ templates['login'] = template({"1":function(container,depth0,helpers,partials,da
 templates['match'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "<div class=\"match\">\r\n\r\n  \r\n  <p>Below is your dinner plan for tonight!</p>\r\n  <fieldset>\r\n  <b> Time: </b> "
+  return "<div class=\"match\">\r\n\r\n  <div class=\"jumbotron text-center\">\r\n	  <p>Below is your dinner plan for tonight!</p>\r\n	  \r\n	  <b> Time: </b> <p>"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.match : depth0)) != null ? stack1.diner_time : stack1), depth0))
-    + " <br>\r\n  <b> Dining Hall:</b> "
+    + ":00</p> <br>\r\n	  <b> Dining Hall:</b> <p>"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.match : depth0)) != null ? stack1.diner_location : stack1), depth0))
-    + " <br>\r\n  <b> You are going to eat and meet with:</b> <p id=\"personEatWith\">"
+    + "</p> <br>\r\n	  <b> You are going to eat and meet with:</b> <p id=\"personEatWith\">"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.match : depth0)) != null ? stack1.dinner_meet : stack1), depth0))
-    + "</p> <br>\r\n  <input class=\"generalBtn\" type=\"button\" id='dinnerCompleteBtn' value=\"Dinner Completed\">\r\n  </fieldset>\r\n  \r\n\r\n</div>\r\n";
+    + "</p> <br>\r\n	  <button class='btn btn-primary btn-lg' id='dinnerCompleteBtn'>Dinner Completed</button>\r\n	  \r\n  </div>\r\n\r\n</div>\r\n";
 },"useData":true});
 templates['networkContainer'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = container.invokePartial(partials.networkMember,depth0,{"name":"networkMember","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+  return ((stack1 = container.invokePartial(partials.networkMember,depth0,{"name":"networkMember","data":data,"indent":"      ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
 },"3":function(container,depth0,helpers,partials,data) {
-<<<<<<< HEAD
-    return "    <p>Nobody in your network :( Add them after meals!</p>\r\n";
-=======
-    return "	<a href=\"#\" class=\"sendMessage\">"
+    return "      <p><a href=\"#\" class=\"sendMessage\">"
     + container.escapeExpression(container.lambda(depth0, depth0))
-    + "</a> <p>no conversation yet</p>\n";
->>>>>>> 5822c8c2134934bb2949adbff95e14606301db0a
+    + "</a></p> <p class=\"messageDetail\">no conversation yet</p>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {};
 
-  return "<div id=\"network\">\r\n  <h2>Eat, Meet, MIT</h2>\r\n  <div class=\"error\"></div>\r\n  <p>Hi there "
+  return "<div id=\"network\">\r\n  <div class=\"jumbotron text-center\">\r\n    <h1>Eat, Meet, MIT</h1>\r\n    <div class=\"error\"></div>\r\n    <p>Hi there "
     + container.escapeExpression(((helper = (helper = helpers.currentUser || (depth0 != null ? depth0.currentUser : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"currentUser","hash":{},"data":data}) : helper)))
-<<<<<<< HEAD
-    + ". Below are all of the people who are a part of your network. Click on their name to send them a message! (<a id=\"logoutLink\" href=\"#\">logout</a>) </p>\r\n\r\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.networkMembers : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
-    + "\r\n  \r\n\r\n  <input id=\"homeBtn\" class=\"generalBtn\" type=\"button\" value=\"Back Home\">\r\n\r\n</div>\r\n";
-=======
-    + ". Below are all of the people who are a part of your network. Click on their name to send them a message! (<a id=\"logoutLink\" href=\"#\">logout</a>) </p>\n\n  <p>You may add friends after each meal!</p>\n\n"
+    + ". Below are all of the people who are a part of your network. Click on their name to send them a message! (<a id=\"logoutLink\" href=\"#\">logout</a>) </p>\r\n    <p>You may add friends after each meal!</p>\r\n    <button id=\"homeBtn\" class=\"btn btn-primary btn-lg\">Back Home</button>\r\n  </div>\r\n  <div class=\"row text-center\">\r\n    <div class=\"col-md-12\">\r\n\r\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.output : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\n"
+    + "\r\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.friend_no_message : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\n  \n\n  <input id=\"homeBtn\" class=\"generalBtn\" type=\"button\" value=\"Back Home\">\n\n</div>\n";
->>>>>>> 5822c8c2134934bb2949adbff95e14606301db0a
+    + "\r\n    </div>\r\n  </div>\r\n</div>\r\n";
 },"usePartial":true,"useData":true});
 templates['networkMember'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression, alias5=container.lambda;
 
   return "<div id=\"networkMember\" data-conversation-id="
-<<<<<<< HEAD
-    + alias1(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"_id","hash":{},"data":data}) : helper)))
-    + ">\r\n  <!-- The \"this\" refers to the name of user that is in your network -->\r\n  <a href=\"#\" class=\"sendMessage\">"
-    + alias1(container.lambda(depth0, depth0))
-    + "</a>\r\n  \r\n</div>\r\n";
-=======
     + alias4(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"_id","hash":{},"data":data}) : helper)))
-    + ">\n  <!-- The \"this\" refers to the name of user that is in your network -->\n  <div class=\"network_messages\">\n  <a href=\"#\" class=\"sendMessage\">"
+    + ">\r\n  <!-- The \"this\" refers to the name of user that is in your network -->\r\n  <div class=\"network_messages\">\r\n	  <p><a href=\"#\" class=\"sendMessage\">"
     + alias4(((helper = (helper = helpers.friend_name || (depth0 != null ? depth0.friend_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"friend_name","hash":{},"data":data}) : helper)))
-    + "</a> \n  <p> "
+    + "</a></p>\r\n	  <p class=\"messageDetail\"> \""
     + alias4(alias5(((stack1 = (depth0 != null ? depth0.last_messasge : depth0)) != null ? stack1.content : stack1), depth0))
-    + " last sent at "
+    + "\"\" last sent at "
     + alias4(alias5(((stack1 = (depth0 != null ? depth0.last_messasge : depth0)) != null ? stack1.create_time : stack1), depth0))
-    + "</p>\n  <p>"
+    + "</p>\r\n	  <p class=\"messageDetail\">"
     + alias4(((helper = (helper = helpers.unread || (depth0 != null ? depth0.unread : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"unread","hash":{},"data":data}) : helper)))
-    + " new messages</p>\n  </div>\n</div>\n";
->>>>>>> 5822c8c2134934bb2949adbff95e14606301db0a
+    + " new messages</p>\r\n  </div>\r\n</div>";
 },"useData":true});
 templates['potentialFriend'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var alias1=container.lambda, alias2=container.escapeExpression;
