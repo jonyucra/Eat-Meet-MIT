57316839067155207a9e022d32d0d80ebc3370a9
Jonatan Yucra Rodriguez <jyucra@mit.edu>
2015-12-06 13:19:14 -0500

    Added specs to requests.js

--- models/requests.js
+++ models/requests.js
@@ -11,16 +11,18 @@ var requestSchema = mongoose.Schema({
 	createdBy: {type: Number, ref: 'User'}
 });
 
-
-//Creates a new request
+/**
+   * Public function. Creates a new request.
+   * @param {Array<Integer>} diningtimes - Array of Integers containing the times a user is willing to eat at.
+   * @param {Array<String>} dininglocations - Array of Strings containing the locations a user is willing to eat at.
+   * @param {String} author - The username of the user who's placing this specific request.
+   * @param {function} callback - Callback function
+**/
 requestSchema.statics.createNewRequest = function (diningtimes, dininglocations, author, callback) {
 
-  /*How will we handle multiple request by the same user?
-  	For the time being just create whatever Request
-  */
-  Request.count({}, function( err, count){
+  Request.count({}, function (err, count){
 
-    User.findOne({username:author}, function (err,docone){
+    User.findOne({username:author}, function (err,userdoc){
       Request.create({
         _id: count,
         dinnerTimes: diningtimes,
@@ -28,12 +30,12 @@ requestSchema.statics.createNewRequest = function (diningtimes, dininglocations,
         diningHalls: dininglocations,
         status: "pending",
         matchedTo: ["No Match"],
-        createdBy: docone._id
+        createdBy: userdoc._id
       }, function (err) {
 
-        Request.findOne({_id: count}, function (err, doctwo){
+        Request.findOne({_id: count}, function (err, requestdoc){
 
-          User.update({username:author}, {$push:{requestHistory:doctwo._id}}, function(err){
+          User.update({username:author}, {$push:{requestHistory:requestdoc._id}}, function(err){
             callback(null);
           })
 
@@ -46,19 +48,33 @@ requestSchema.statics.createNewRequest = function (diningtimes, dininglocations,
 
 }
 
+/**
+   * Private function. Updates requests collection after a match is made.
+   * @param {Integer} authorrequestid - The id of the request from the user who is currently logged in.
+   * @param {Integer} otherrequestid - The id of the request from the user who the current user matched to.
+   * @param {String} placematch - The dining hall the two matched users have in common.
+   * @param {Integer} timematch - The time the two matched users have in common.
+   * @param {String} authorname - The username of the user who is currently logged in.
+   * @param {String} othername -  The username of the user who the current user matched to.
+   * @param {String} authoremail -  The email of the user who is currently logged in.
+   * @param {String} otheremail - The email of the user who the current user matched to.
+   * @param {function} callback -  Callback function.
+**/
+var updateAfterMatch = function (authorrequestid, otherrequestid, placematch, timematch, authorname, othername, authoremail, otheremail, callback) {
 
-
-//Updates the request documents specified and changes their status to matched
-var updateAfterMatch = function (firstrequestid, secondrequestid, placematch, timematch, firstperson, secondperson, authoremail, otheremail, callback) {
-
-  Request.update({_id: firstrequestid}, { status: "matched", matchedTo: [timematch, placematch, secondperson, authoremail, otheremail] }, function (err, firstreq) {
-    Request.update({_id: secondrequestid}, {status: "matched", matchedTo: [timematch, placematch, firstperson, otheremail, authoremail] }, function (err, secreq) { //Can I do these two in one line?
+  Request.update({_id: authorrequestid}, { status: "matched", matchedTo: [timematch, placematch, othername, authoremail, otheremail] }, function (err, firstreq) {
+    Request.update({_id: otherrequestid}, {status: "matched", matchedTo: [timematch, placematch, authorname, otheremail, authoremail] }, function (err, secreq) { 
       callback(null);
     });
   });
 }
 
-//returns matching dining hall and time
+/**
+   * Private function. Gets a dininghall and time that two specified requests have in common.
+   * @param {Integer} firstrequestid - The id of the request from the user who is currently logged in.
+   * @param {Integer} secondrequestid - The id of the request from the user who the current user matched to.
+   * @param {function} callback -  Callback function.
+**/
 var getTimeAndLocation = function (firstrequestid, secondrequestid, callback) {
 
   Request.findOne({_id:firstrequestid}, function (err, firstdoc) {
@@ -86,7 +102,11 @@ var getTimeAndLocation = function (firstrequestid, secondrequestid, callback) {
 
 }
 
-//clears both users requests after a successful dinner (sets them to inactive)
+/**
+   * Public function. Sets a pair of matched requests to inactive status.
+   * @param {String} currentuser - The username of the user who is currently logged in.
+   * @param {function} callback -  Callback function.
+**/
 requestSchema.statics.clearMatch = function (currentuser, callback) {
   User.findOne({username: currentuser}, function (err, authoruser){
     Request.findOne({_id:authoruser.requestHistory[authoruser.requestHistory.length-1]}, function (err, authorrequest){
@@ -101,15 +121,19 @@ requestSchema.statics.clearMatch = function (currentuser, callback) {
   });
 }
 
-//matches a user to the earliest timestamped request that fits the bill
+/**
+   * Public function. Matches a request to the earliest timestamped request that fits the bill, if the user's status allows for it.
+   * @param {String} currentuser - The username of the user who is currently logged in.
+   * @param {function} callback -  Callback function.
+**/
 requestSchema.statics.getMatch = function (currentuser, callback) {
 
   User.findOne({username: currentuser}, function (err, doc){
     if( err ){
       callback(true);
-    } else if (doc) { //there is a user found
+    } else if (doc) { //current user exists
 
-      if (doc.requestHistory.length>0) { //If there is at least one request in the history (i.e. it's not the user's first time signing in)
+      if (doc.requestHistory.length>0) { //There is at least one request in the history
 
         latestRequest = doc.requestHistory[doc.requestHistory.length-1];
 
@@ -118,11 +142,11 @@ requestSchema.statics.getMatch = function (currentuser, callback) {
             callback(true);
           } else {
 
-            if ( doclatest.status == "inactive" ){
-              callback(null,null,null); // this is just so that request is not recognized and it routes to "you have no inactive requests"
-            } else if (doclatest.status == "matched") { 
+            if ( doclatest.status == "inactive" ){ //Request is not recognized. i.e. "You have no requests, make one."
+              callback(null,null,null); 
+            } else if (doclatest.status == "matched") { //Latest request is a matched request.
               callback(null, {status: "matched"} , {diner_time: doclatest.matchedTo[0], diner_location: doclatest.matchedTo[1], dinner_meet: doclatest.matchedTo[2], user_email: doclatest.matchedTo[3], other_email: doclatest.matchedTo[4]});
-            } else {
+            } else { //You're making a new request
 
               latestDining = doclatest.diningHalls;
               latestTimes = doclatest.dinnerTimes;
@@ -130,9 +154,9 @@ requestSchema.statics.getMatch = function (currentuser, callback) {
 
                 if (err) {
                   callback(true);
-                } else if (docs.length == 0){
+                } else if (docs.length == 0){ // There is no request which matches yours
                   callback(null, {status: "pending"}, null);
-                } else{
+                } else{ //There is a matching request
                   var earliestRequest = docs[0];
                   var earliestStamp = docs[0].timestamp;
                   docs.forEach(function(e){
@@ -163,17 +187,22 @@ requestSchema.statics.getMatch = function (currentuser, callback) {
         });
 
       } else {
-        callback(null, null, null);
+        callback(null, null, null); //User has no request history
       }
 
     } else {
-      callback("No user found");
+      callback("No user found.");
     }
 
   });
 
 }
 
+/**
+   * Public function. Sets a request to inactive, effectively cancelling it.
+   * @param {String} currentuser - The username of the user who is currently logged in.
+   * @param {function} callback -  Callback function.
+**/
 requestSchema.statics.cancelRequest = function (currentuser, callback){
   User.findOne({username:currentuser}, function (err, docone){
     Request.findOne({_id:docone.requestHistory[docone.requestHistory.length-1]}, function (err, authorrequest){
