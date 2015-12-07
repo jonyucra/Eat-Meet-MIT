c20b6fd90459607b429e2cf120f9c4723cd47804
Jonatan Yucra Rodriguez <jyucra@mit.edu>
2015-12-06 21:16:16 -0500

    Uncommenting something for testing purposes.

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -65,13 +65,14 @@ var loadNetwork = function() {
   $(document).on('click', '#makeRequest', function(evt) {
     $.get("/requests/suggestion", function (response) {
       loadPage('request', {currentUser: currentUser, diningHall: response.content.diningHall, diningTime:response.content.dinnerTime});
-
+      /*
       var dt = new Date();
       $("input:checkbox[name=time]").each(function(){
            if ( $(this).val() <= dt.getHours() ) {
                $(this).prop('disabled', true);
            }
       });
+      */
     })
       
   }); 
